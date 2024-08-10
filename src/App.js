72f78bb0ef77364a06b4f335f5743440dcae9312
Mark Mulli <markmulli001@gmail.com>
2024-08-10 14:06:33 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 import "./styles.css"
 
@@ -14,23 +15,26 @@ export default function App() {
         const num_array = []
         for (let i = 0; i < 10; i++) {
             const random_number = 1 + Math.floor(Math.random() * 6)
-            num_array[i] = random_number
+            num_array[i] = {id:nanoid(), value: random_number, isHeld:false}
         }
         return num_array
     }
     
+
     const [newDice, setNewDice] = React.useState(allNewDice())
 
     function reroll() {
         setNewDice(allNewDice())
     }
 
+    const diceElements = newDice.map((number) => {
+        return <Die key={number.id} value = {number.value} isHeld = {number.isHeld}/>
+    }) 
+
     return (
         <main>
             <div className="dice-container">
-                { newDice.map((number) => {
-                    return <Die  value = {number}/>
-                }) }
+                {diceElements}
             </div>
 
             <button className="roll-dice" onClick= {reroll}>Roll</button>
