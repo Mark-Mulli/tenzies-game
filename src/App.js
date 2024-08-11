593f052c9c018c9d530553a7daf3a142cae256b3
Mark Mulli <markmulli001@gmail.com>
2024-08-11 20:52:42 +0300

    confetti

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
 
+import Confetti from "react-confetti";
+
 import "./styles.css"
 
 
@@ -11,33 +13,73 @@ import "./styles.css"
 
 export default function App() {
 
+    function generateNewDie() {
+        return {
+            id:nanoid(), value: 1 + Math.floor(Math.random() * 6), isHeld:false
+        }
+    }
+
     function allNewDice() {
         const num_array = []
         for (let i = 0; i < 10; i++) {
-            const random_number = 1 + Math.floor(Math.random() * 6)
-            num_array[i] = {id:nanoid(), value: random_number, isHeld:false}
+            num_array[i] = generateNewDie()
         }
         return num_array
     }
     
+    
 
     const [newDice, setNewDice] = React.useState(allNewDice())
 
+    const [tenzies, setTenzies] = React.useState(false)
+
+
     function reroll() {
-        setNewDice(allNewDice())
+        if (!tenzies) {
+            setNewDice(oldDice => oldDice.map((dice) => {
+                    return dice.isHeld === true ? dice : generateNewDie()
+                }))        
+        } else {
+            setTenzies(false)
+            setNewDice(allNewDice())
+
+        }
+    }
+
+    // console.log(newDice)
+
+
+    React.useEffect(() => {
+        const allHeld = newDice.every(dice => dice.isHeld)
+        const allSameVal = newDice.every(dice => dice.value === newDice[0].value)
+        if (allHeld && allSameVal) {
+            setTenzies(true)
+            console.log("You Won")
+        }
+
+    }, [newDice])
+
+    function hold(id) {
+        setNewDice(oldDice => oldDice.map((dice) => {
+            return dice.id === id ? {...dice, isHeld: !dice.isHeld} : dice
+        }))
     }
 
     const diceElements = newDice.map((number) => {
-        return <Die key={number.id} value = {number.value} isHeld = {number.isHeld}/>
+        return <Die key={number.id} value = {number.value} isHeld = {number.isHeld} hold = {() => hold(number.id)}/>
     }) 
 
     return (
         <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+
             <div className="dice-container">
                 {diceElements}
             </div>
 
-            <button className="roll-dice" onClick= {reroll}>Roll</button>
+            <button className="roll-dice" onClick= {reroll}>{tenzies ? "New Game": "Roll"}</button>
         </main>
     )
 }
--- src/Die.js
+++ src/Die.js
@@ -7,7 +7,7 @@ export default function Die(props) {
     }
 
     return (
-        <div className="die-face" style={styles}>
+        <div className="die-face" style={styles} onClick={props.hold}>
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
